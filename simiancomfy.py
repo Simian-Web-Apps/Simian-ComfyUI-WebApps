4037cc049d682c90b04c0f4dd825fdfede638ec9
Paul.Rutten <paul.rutten@monkeyproofsolutions.nl>
2026-05-01 17:13:20 +0200

    Add ComfyUI webapp nodes to Simian WebApp conversion functionality Add template for new apps

---
+++ simiancomfy.py
@@ -0,0 +1,477 @@
+"""Convert ComfyUI API workflow containing webapp API nodes to a Simian web app."""
+
+import base64
+import glob
+import io
+import json
+import math
+import os
+import re
+import sys
+from typing import Sequence
+from urllib import request
+
+from PIL import Image, ImageDraw
+
+sys.path.append(r"C:\Files\_webframework\python\Implementation\simian-gui")
+sys.path.append(r"C:\Files\_webframework\python\Implementation\simian-local")
+sys.path.append(r"C:\Programs\ComfyUI\ComfyUI_windows_portable\ComfyUI\custom_nodes\ComfyUI_webapp")
+
+import plotly.graph_objects as go
+from interpret_json import process_workflow_api
+from simian.gui import Form, component, component_properties, utils
+
+CONFIG = {}
+
+CONFIG["group_nesting"] = ["panels"]
+CONFIG["panels_collapsible"] = True
+CONFIG["save_intermediates"] = True
+
+DETECTED_FILES = {
+    "workflow": None,
+    "simian_config": None,
+}
+
+
+# TODO: put Advanced components in own subpanels(?) or just hide until a central(?) tickbox is ticked.
+# TODO: process tooltips
+# TODO: show created images somewhere and allow downloading?
+# TODO: Connect with web sockets + extra configs
+
+GROUP_TYPES = {
+    "tabs": component.Tabs,
+    "columns": component.Columns,
+    "panels": component.Panel,
+}
+COMPONENT_DICT = {
+    "WebApp_Description": component.HtmlElement,
+    "WebApp_Integerinput": component.Number,
+    "WebApp_FloatInput": component.Number,
+}
+
+
+def add_group(type_str: str, group_dict, parent) -> component.Component:
+    if type_str == "panels":
+        # Always a new one
+        new_comp = component.Panel(key=f"comp_{group_dict['id']}", parent=parent)
+        new_comp.title = group_dict["_meta"]["title"]
+        new_comp.collapsible = CONFIG["panels_collapsible"]
+
+    elif type_str == "tabs":
+        if isinstance(parent.components[-1], component.Tabs):
+            tab_comp = parent.components[-1]
+        else:
+            tab_comp = component.Tabs(key=f"comp_{group_dict['id']}", parent=parent)
+
+        new_comp = tab_comp.addTab(group_dict["_meta"]["title"], key=f"tab{group_dict['id']}")
+
+    elif type_str == "columns":
+        if isinstance(col_comp := parent.components[-1], component.Columns):
+            # col_comp = parent.components[-1]
+            new_width = math.floor(12 / (len(col_comp.columns) + 1))
+
+            for col in col_comp.columns:
+                col.width = new_width
+        else:
+            col_comp = component.Columns(key=f"comp_{group_dict['id']}", parent=parent)
+            new_width = 6
+
+        new_col = component_properties.Column(new_width)
+        new_comp = col_comp.addColumn(new_col)
+
+        descr = component.HtmlElement(f"label_{group_dict['id']}", parent=new_col)
+        descr.content = group_dict["_meta"]["title"]
+
+    return new_comp
+
+
+def create_comp(node: dict) -> component.Component:
+    """Create a Simian componenf from a ComfyUI webapp node.
+
+    Args:
+        node: ComfyUI webapp node definition.
+
+    Returns:
+        The Simian component that was created from the ComfyUI definition.
+    """
+    if node["class_type"] == "WebApp_Description":
+        # Description.
+        new_comp = component.HtmlElement(f"comp_{node['id']}")
+        new_comp.content = node["inputs"]["description"]
+
+    elif node["class_type"] in ["WebApp_Integerinput", "WebApp_Floatinput"]:
+        # Numeric input
+        new_comp = component.Number(f"comp_{node['id']}")
+        new_comp.label = node["_meta"]["title"]
+        new_comp.defaultValue = node["inputs"].get("default", None)
+        component_properties.Validate.create(
+            new_comp,
+            min=node["inputs"].get("minimum", None),
+            max=node["inputs"].get("maximum", None),
+        )
+        if node["class_type"] == "WebApp_Integerinput":
+            new_comp.decimalLimit = 0
+
+    elif node["class_type"] == "WebApp_BooleanInput":
+        # Boolean input.
+        new_comp = component.Checkbox(f"comp_{node['id']}")
+        new_comp.label = node["_meta"]["title"]
+        new_comp.defaultValue = node["inputs"].get("default", None)
+
+    elif node["class_type"] == "WebApp_Stringinput":
+        # Text input.
+        if node["inputs"]["mode"] == "Multiline":
+            new_class = component.TextArea
+        else:
+            new_class = component.TextField
+
+        new_comp = new_class(f"comp_{node['id']}")
+        new_comp.label = node["_meta"]["title"]
+        new_comp.defaultValue = node["inputs"].get("default", None)
+
+    elif node["class_type"] == "WebApp_Selectioninput":
+        # Select component
+        options = node["inputs"].get("options", [])
+        new_comp = component.Select(f"comp_{node['id']}")
+        new_comp.label = node["_meta"]["title"]
+        new_comp.setValues(
+            labels=options, values=options, default=node["inputs"].get("default", None)
+        )
+        new_comp.multiple = node["inputs"].get("maximum", 1) > 1
+
+    elif node["class_type"] == "WebApp_MaskedImageinput":
+        # Masked image input.
+        new_comp = component.Panel(f"cont_{node['id']}")
+        new_comp.label = node["_meta"]["title"]
+        new_comp.collapsible = CONFIG["panels_collapsible"]
+
+        if node["inputs"]["is_image_used"]:
+            upload_image = component.File(f"upload_{node['id']}", parent=new_comp)
+            upload_image.filePattern = "image/*"
+
+        plot_obj = component.Plotly(f"plot_{node['id']}", parent=new_comp)
+        plot_obj.figure = go.Figure()
+        plot_obj.figure.update_layout(margin={"l": 0, "r": 0, "t": 0, "b": 0}, dragmode=False)
+        plot_obj.aspectRatio = 2
+        plot_obj.defaultValue["config"].update(
+            {"modeBarButtonsToRemove": ["pan", "zoom", "zoomin", "zoomout"]}
+        )
+        plot_obj.customConditional = f"show = row.upload_{node['id']}.length > 0"
+
+        # Use the image selected in the File component as background in the Plotly component.
+        plot_obj.calculateValue = (
+            "var new_value = value; var comp = row.upload_"
+            + node["id"]
+            + "; if (comp.length > 0) { new_value.layout.images = [{'source': comp[0].url, "
+            + "'x': 0.5, 'y': 0.5, 'xref': 'paper', 'yref': 'paper', 'xanchor': 'center', "
+            + "'yanchor': 'middle', 'sizex': 1, 'sizey': 1, 'layer': 'below'}]; "
+            + "new_value.layout.xaxis = {'range': [0, 1], 'visible': false}; "
+            + "new_value.layout.yaxis = {'range': [0, 1], 'visible': false};} "
+            + "else if (new_value?.layout === undefined) {} else {new_value.layout.images = []}; "
+            + "value = new_value;"
+        )
+
+        if node["inputs"]["is_mask_used"]:
+            # The image may be masked. Enable drawing shapes in the image.
+            plot_obj.figure.update_layout(
+                dragmode="drawrect",
+                newshape_line_color="black",
+                newshape_fillcolor="white",
+                newshape_opacity=0.75,
+            )
+
+            plot_obj.defaultValue["config"].update(
+                {
+                    "modeBarButtonsToAdd": [
+                        "drawclosedpath",
+                        "drawcircle",
+                        "drawrect",
+                        "eraseshape",
+                    ]
+                }
+            )
+
+    else:
+        new_comp = None
+
+    return new_comp
+
+
+def scan_resources(calling_file: str):
+    """Scan the surroundings of the given file for Comfy - Simian specific files.
+
+    Args:
+        calling_file: File to scan the surroundings of.
+    """
+    app_folder = os.path.dirname(calling_file)
+    folder_jsons = glob.glob("*.json", root_dir=app_folder)
+
+    for file in folder_jsons:
+        full_file = os.path.join(app_folder, file)
+        with open(full_file, "r") as f:
+            try:
+                json_dict = json.load(f)
+            except Exception:
+                continue
+
+        if "inputs" in list(json_dict.values())[0]:
+            DETECTED_FILES["workflow"] = full_file
+        elif "rootlevel" in json_dict:
+            DETECTED_FILES["simian_config"] = full_file
+
+
+def convert_api_to_app(form: Form) -> None:
+    """Convert ComfyUI workflow API with webapp nodes to a Simian web app.
+
+    Args:
+        form: Simian Form to add the components to.
+    """
+    if DETECTED_FILES["workflow"] is not None:
+        app_nodes_dict = process_workflow_api(DETECTED_FILES["workflow"])
+
+        if CONFIG.get("save_intermediates", False):
+            with open(
+                os.path.join(os.path.dirname(DETECTED_FILES["workflow"]), "interpreted.json"), "w+"
+            ) as f:
+                json.dump(app_nodes_dict, f)
+
+        root_cont = app_nodes_dict.pop("root", [])
+
+        has_run_button = any([node["class_type"] == "WebApp_Button" for node in root_cont])
+
+        for node_dict in root_cont:
+            node_dict_to_component(app_nodes_dict, node_dict, 0, form)
+
+        # When no RunWorkflow button added in the definition, add one here.
+        if not has_run_button:
+            button = component.Button("run_workflow", form)
+            button.label = "Run workflow"
+            button.setEvent("RunWorkflow")
+    else:
+        descr = component.HtmlElement("NoResourcesDetected", form)
+        descr.content = "No workflow API detected."
+
+
+def node_dict_to_component(all_nodes: dict, node: dict, level: int, parent):
+    if (new_node := create_comp(node)) is not None:
+        parent.addComponent(new_node)
+
+    elif node["class_type"] == "WebApp_Grouping":
+        if level + 1 <= len(CONFIG["group_nesting"]):
+            group_type = CONFIG["group_nesting"][level]
+        else:
+            group_type = "panels"
+
+        new_sub_comp = add_group(type_str=group_type, group_dict=node, parent=parent)
+
+        # TODO: what if parent is a Panel and we are adding a DataGrid with comps. Remove the panel, as not needed for grouping
+
+        child_nodes = all_nodes[node["id"]]
+
+        if node["inputs"]["mode"] != "Repeating":
+            pass
+
+        # elif len(child_nodes) == 1:
+        #     new_node = create_comp(child_nodes[0])
+        #     if hasattr(new_node, "multiple"):
+        #         new_node.multiple = True
+        #         component_properties.Validate.create(
+        #             new_node,
+        #             minLength=node["inputs"].get("mode.minimum", 1),
+        #             maxLength=node["inputs"].get("mode.maximum", 1),
+        #         )
+        #         new_sub_comp.addComponent(new_node)
+        #         return
+        #     else:
+        #         new_sub_comp = component.DataGrid(key=f"rep_{node['id']}", parent=new_sub_comp)
+        #         component_properties.Validate.create(
+        #             new_sub_comp,
+        #             minLength=node["inputs"].get("mode.minimum", 1),
+        #             maxLength=node["inputs"].get("mode.maximum", 1),
+        #         )
+
+        else:
+            component.Hidden(key=f"hid_{node['id']}", parent=new_sub_comp)
+
+            assert not isinstance(new_sub_comp, component.DataGrid), (
+                "Nesting Repeated Groupings is not supported yet."
+            )
+
+            new_sub_comp = component.DataGrid(key=f"rep_{node['id']}", parent=new_sub_comp)
+            component_properties.Validate.create(
+                new_sub_comp,
+                minLength=node["inputs"].get("mode.minimum", 1),
+                maxLength=node["inputs"].get("mode.maximum", 2),
+            )
+
+        for child in child_nodes:
+            node_dict_to_component(all_nodes, child, level=level + 1, parent=new_sub_comp)
+
+    else:
+        print(node)
+
+
+def gui_init(meta_data: dict) -> dict:
+    # Create the form and load the json builder into it.
+    form = Form()
+
+    convert_api_to_app(form)
+
+    if DETECTED_FILES["workflow"] is not None and CONFIG.get("save_intermediates", False):
+        with open(
+            os.path.join(os.path.dirname(DETECTED_FILES["workflow"]), "created_form.json"),
+            "w+",
+        ) as f:
+            print(form.jsonEncode(), file=f)
+
+    return {"form": form}
+
+
+def gui_event(meta_data: dict, payload: dict) -> dict:
+    Form.eventHandler(RunWorkflow=run_workflow)
+    callback = utils.getEventFunction(meta_data, payload)
+    return callback(meta_data, payload)
+
+
+def run_workflow(_meta_data, payload):
+    print(payload["submission"]["data"])
+
+    with open(DETECTED_FILES["workflow"]) as f:
+        prompt = json.load(f)
+
+    process_component_values(prompt, payload["submission"]["data"])
+
+    if CONFIG.get("save_intermediates", False):
+        # Save the prompt we are about to send to a local file for troubleshooting.
+        with open(
+            os.path.join(os.path.dirname(DETECTED_FILES["workflow"]), "sent_prompt.json"), "w+"
+        ) as f:
+            json.dump(prompt, f)
+
+    data = json.dumps({"prompt": prompt}).encode("utf-8")
+    req = request.Request("http://127.0.0.1:8188/prompt", data=data)
+    request.urlopen(req)
+
+    return payload
+
+
+def process_component_values(prompt: dict, data: dict):
+    """Process the values selected in the app and put them in the prompt.
+
+    Args:
+        prompt: dictionary with the workflow API that we need to fill with the app values.
+        data: dictionary with app values to insert into the prompt.
+    """
+    app_nodes_dict = process_workflow_api(DETECTED_FILES["workflow"])
+
+    # Insert the values from the app.
+    for comp_id, value in data.items():
+        match_id = re.search(r"\d+$", comp_id)
+
+        if match_id is None:
+            # The workflow API does not contain an item with the same identifier as the component.
+            # Nothing to put the value in. Skip
+            continue
+        workflow_id = match_id.group()
+
+        if isinstance(value, str) or not isinstance(value, Sequence):
+            # Value must always be in a list. This eases further processing.
+            value = [value]
+
+        if workflow_id in app_nodes_dict:
+            # Group with sub-components,
+
+            # Process the values in the rows at the same time; they need to be put in the same part of the prompt.
+            process_component_values(prompt, {k: [dic[k] for dic in value] for k in value[0]})
+
+        elif prompt[workflow_id]["class_type"] == "WebApp_MaskedImageinput":
+            # Dealing with an image
+            if comp_id.startswith("upload") and len(value) > 0:
+                # Get the base64 encoded representation of the image.
+                prompt[workflow_id]["inputs"]["image_base64"] = json.dumps(
+                    [process_image_to_str(item) for item in value]
+                )
+            elif comp_id.startswith("plot") and len(value) > 0:
+                # Get the base64 encoded representation of the mask.
+                prompt[workflow_id]["inputs"]["mask_base64"] = json.dumps(
+                    [process_mask_to_str(item) for item in value]
+                )
+            else:
+                # Component does not contain a value to put in the prompt.
+                pass
+        else:
+            prompt[workflow_id]["inputs"]["value_from_interface"] = json.dumps(value)
+
+
+def process_image_to_str(file_data: list[dict] | list[dict]) -> str:
+    """Process the image file selected in the File component.
+
+    Args:
+        file_data: Meta data of the selected file.
+
+    Returns:
+        Base64 encoded string of the selected image file.
+    """
+    if isinstance(file_data, list):
+        file_data = file_data[0]
+
+    url = file_data.get("url", "")
+
+    return re.sub(r"^data:image/\w+;base64,", "", url)
+
+
+def process_mask_to_str(plot_data: dict) -> str:
+    """Process the image mask that was drawn in the Plotly component.
+
+    Args:
+        plot_data: Contents of the masked image.
+
+    Returns:
+        Base64 encoded string of the image mask.
+    """
+    plot_obj = utils.Plotly(payload_value=plot_data)
+    backgrounds = plot_obj.layout.get("images", [])
+    shapes = plot_obj.getShapes()
+
+    if len(backgrounds) == 0 or len(shapes) == 0:
+        # No background image or no shapes drawn.
+        # mask_image = Image.new("L", (100, 100), 0)
+        return ""
+
+    else:
+        source_str = re.sub(r"^data:image/\w+;base64,", "", backgrounds[0].get("source", ""))
+        decoded_bytes = base64.b64decode(source_str)
+        byte_stream = io.BytesIO(decoded_bytes)
+        im = Image.open(byte_stream)
+
+        new_image = Image.new("1", im.size, 0)
+        fill_val = 1
+        mask_image = ImageDraw.Draw(new_image)
+
+        # Both axes have a range of [0, 1], with a plot aspect ratio of 2. Below the coordinates
+        # are converted to pixels so that we can draw the mask shapes and convert it to a string.
+        unit_size_x = max([im.size[0], 2 * im.size[1]])
+        unit_size_y = max([im.size[0] / 2, im.size[1]])
+        offset = [(unit_size_x - im.size[0]) / 2, (unit_size_y - im.size[1]) / 2]
+
+        for shape in shapes:
+            coords = list(
+                zip(
+                    sorted([x * unit_size_x - offset[0] for x in shape["x"]]),
+                    sorted([im.size[1] - y * unit_size_y + offset[1] for y in shape["y"]]),
+                )
+            )
+            if shape["type"] == "rect":
+                mask_image.rectangle(coords, fill_val, 1)
+            elif shape["type"] == "circle":
+                mask_image.ellipse(coords, fill_val, 1)
+            elif shape["type"] == "path":
+                mask_image.polygon(coords, fill_val, 1)
+
+    buffered = io.BytesIO()
+    new_image.save(buffered, format="jpeg")
+
+    if CONFIG.get("save_intermediates", False):
+        new_image.save(os.path.join(os.path.dirname(DETECTED_FILES["workflow"]), "mask.png"))
+
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
---
+++ template/comfy_webapp.py
@@ -0,0 +1,12 @@
+
+
+from simian.local import run
+from simiancomfy import gui_init, gui_event, CONFIG, scan_resources
+
+# Look for files in the local folder.
+scan_resources(__file__)
+
+CONFIG["group_nesting"] = ["tabs"]
+
+if __name__ == "__main__":
+    run("comfy_webapp", debug=True, show_refresh=True)
